10c5322b71704eef6fb06818f0e1370c4b08c59c
DHANUSH TALLURU <dhanush.talluru@gmail.com>
2025-07-03 12:36:43 +0530

    Create firstProgramInJava.java

---
+++ firstProgramInJava.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class firstProgramInJava {
+  public static void main (String args[]) {
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    System.out.println(name+", welcome to the java programming language");
+  }
