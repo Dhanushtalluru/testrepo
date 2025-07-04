23095769160a4f9341776fa946649cd21b4854ba
DHANUSH TALLURU <dhanush.talluru@gmail.com>
2025-07-04 10:52:51 +0530

    Update firstProgramInJava.java
    Added the sum of two numbers program to this code.

--- firstProgramInJava.java
+++ firstProgramInJava.java
@@ -1,7 +1,16 @@
 import java.util.Scanner;
-class firstProgramInJava {
-  public static void main (String args[]) {
+class Main {
+  public static void firstProgramInJava (String args[]) {  
     Scanner sc = new Scanner(System.in);
     String name = sc.nextLine();
     System.out.println(name+", welcome to the java programming language");
+    System.out.println("sum of a and b");  
+    System.out.println("enter number a: ");
+    int a = sc.nextInt();
+    System.out.println("enter number b: ");
+    int b = sc.nextInt();
+    System.out.println(a+b);
+    sc.close();
+    System.out.println("end of program");
   }
+}
