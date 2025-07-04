5d3d69f12fe8d7dce16ad389935f26172aa67de9
DHANUSH TALLURU <dhanush.talluru@gmail.com>
2025-07-04 11:05:21 +0530

    Update firstProgramInJava.java
    main method error rectified.

--- firstProgramInJava.java
+++ firstProgramInJava.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class Main {
-  public static void firstProgramInJava (String args[]) {  
+  public static void main (String args[]) {  
     Scanner sc = new Scanner(System.in);
     String name = sc.nextLine();
     System.out.println(name+", welcome to the java programming language");
