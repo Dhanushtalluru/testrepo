bd074629f65d66716d7ecf078f94e45d95a201e9
Dhanushtalluru <dhanush.talluru@gmail.com>
2025-07-09 18:50:59 +0530

    added a addition function

--- firstProgramInJava.java
+++ firstProgramInJava.java
@@ -1,16 +1,12 @@
 import java.util.Scanner;
 class Main {
+  public static void addition(int a,int b){
+    System.out.println(a+b);
+  }
   public static void main (String args[]) {  
     Scanner sc = new Scanner(System.in);
-    String name = sc.nextLine();
-    System.out.println(name+", welcome to the java programming language");
-    System.out.println("sum of a and b");  
-    System.out.println("enter number a: ");
     int a = sc.nextInt();
-    System.out.println("enter number b: ");
     int b = sc.nextInt();
-    System.out.println(a+b);
-    sc.close();
-    System.out.println("end of program");
+    addition(a,b);
   }
 }
